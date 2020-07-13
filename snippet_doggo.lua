c560697791e68990120fac2698e0b8419d8dcf4c
$Peaches from $Cans <kurdt94@gmail.com>
2020-07-13 06:12:48 +0200

    Create snippet_doggo.lua

---
+++ snippet_doggo.lua
@@ -0,0 +1,34 @@
+function modelrequest( model )
+    Citizen.CreateThread(function()
+        RequestModel( model )
+    end)
+end
+
+local dog_spawned = false
+RegisterCommand("doggo", function(source, args, rawCommand)
+    if dog_spawned ~= false then
+        SetEntityAsMissionEntity(dog_spawned, true, true)
+        DeletePed(dog_spawned)
+    end
+    local player = PlayerPedId()
+    local pCoords = GetEntityCoords(player)
+    local offset = GetOffsetFromEntityInWorldCoords(player,0.0,5.0,0.0)
+
+    local a, groundZ = GetGroundZAndNormalFor_3dCoord( offset.x, offset.y, offset.z + 10 )
+
+    local pedModel = GetHashKey("A_C_DogRufus_01")
+    while not HasModelLoaded(pedModel) do
+        Wait(500)
+        print("loading doggo")
+        modelrequest(pedModel)
+    end
+    local created_ped = CreatePed(pedModel,offset.x,offset.y,groundZ,180.0,true,false)
+    while not DoesEntityExist(created_ped) do
+      Wait(300)
+    end
+    Citizen.InvokeNative(0x283978A15512B2FE, created_ped, true)
+    print("new doggo : ".. created_ped)
+    dog_spawned = created_ped
+    Citizen.InvokeNative(0x304AE42E357B8C7E, created_ped, PlayerPedId(),6.00,0.0,0.0,-1,-1,15.0,true,true,false,true,true)
+    SetPedAsGroupMember(created_ped, GetPedGroupIndex(PlayerPedId()))
+end, false)
